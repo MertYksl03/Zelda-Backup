f0bb673131a1b62c658c55be8202a4842fdf301e
MertYksl03 <mert.yksl.03@gmail.com>
2025-01-12 11:28:59 +0300

    first commit

---
+++ backup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Backup starting..."
+
+is_botw_copied=false
+is_totk_copied=false
+is_git_succesful=false
+
+echo "Copying Breath of the Wild  save files"
+	cp -r /home/mert/.var/app/org.ryujinx.Ryujinx/config/Ryujinx/bis/user/save/0000000000000002 Breath-of-the-Wild
+	if [[ $? -eq 0 ]]; then
+		is_botw_copied=true
+	else
+	    echo "Failed to copy BOTW"
+	fi
+
+echo "Copying Tears of the Kingdom save files"
+	cp -r /home/mert/.var/app/org.ryujinx.Ryujinx/config/Ryujinx/bis/user/save/0000000000000003 Tears-of-the-Kingdom
+	#check and prints if the operation is succes
+	if [[ $? -eq 0 ]]; then
+		is_totk_copied=true
+	else
+	    echo "Failed to copy TOTK"
+	fi
+
+if [[ $is_botw_copied = true  && $is_totk_copied = true ]]; then
+	echo "Both games copied succesfully"
+	echo "Uploading files to github"
+	#update the
+	date +"%Y-%m-%d %H:%M:%S" > temp_file
+	cat BACKUP-HISTORY.md >> temp_file
+	mv temp_file BACKUP-HISTORY.md
+fi
