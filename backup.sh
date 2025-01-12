a4cd7bb8fea25960c326de1a06dfdd1423ae9b51
MertYksl03 <mert.yksl.03@gmail.com>
2025-01-12 12:27:53 +0300

    2025-01-12 12:27:53

--- backup.sh
+++ backup.sh
@@ -44,7 +44,8 @@ if [[ $is_botw_copied = true  && $is_totk_copied = true ]]; then
 
 	# Write the content to the temporary file
 	{
-	    echo "-- $backuptime -- $'\n'" &&
+	    echo "-- $backuptime -- " &&
+	    echo &&
 	    cat README.md
 	} > "$temp_file" || { echo "Failed to prepare content"; rm "$temp_file"; exit 1; }
 
